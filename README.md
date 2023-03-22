7f50de97e3a9fe7d47b92ece59251e68642e38f5
artemkuskin <artemkuskin7@gmail.com>
2023-03-22 17:53:49 +0300

    result

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ This repository serves as a template for working with `"playwright"`
 
 All modules with tests are placed in the `"tests"` folder
 
+## creating a test using `Codegen`
+
+```
+npx playwright codegen "URL"
+```
+
 ## Running all tests
 
 ```
@@ -13,13 +19,13 @@ All modules with tests are placed in the `"tests"` folder
 ## Running a single text file
 
 ```
-npx playwright test landing-page.spec.ts
+npx playwright test links.spec.ts
 ```
 
 ## Run a set of test files
 
 ```
-npx playwright test tests/todo-page/ tests/landing-page/
+npx playwright test tests/links.spec.ts tests/search.spec.ts
 ```
 
 ## Run files that have landing or login in the file name
@@ -31,19 +37,33 @@ npx playwright test landing login
 ## Run the test with the title
 
 ```
-npx playwright test -g "add a todo item"
+npx playwright test -g "test search"
 ```
 
 ## Running tests in headed mode
 
 ```
-npx playwright test landing-page.spec.ts --headed
+npx playwright test links.spec.ts --headed
 ```
 
 ## Running tests on a specific project
 
 ```
 npx playwright test landing-page.ts --project=chromium
+```
+
+# Debugging tests
+
+## Debugging all tests
+
+```
+npx playwright test --debug
+```
+
+## Debugging one test file
+
+```
+npx playwright test search.spec.ts --debug
 ```
 
 ## View the report
---
+++ tests/auto-test.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+test.afterEach(async ({ page }, testInfo) => {
+  if (testInfo.status !== testInfo.expectedStatus) {
+    // Get a unique place for the screenshot.
+    const screenshotPath = testInfo.outputPath(`failure.png`);
+    // Add it to the report.
+    testInfo.attachments.push({ name: "screenshot", path: screenshotPath, contentType: "image/png" });
+    // Take the screenshot itself.
+    await page.screenshot({ path: screenshotPath, timeout: 5000 });
+  }
+});
+
+test("test", async ({ page }) => {
+  await page.goto("https://github.com/");
+  await page.getByRole("link", { name: "Sign in" }).click();
+  await page.getByLabel("Username or email address").click();
+  await page.getByLabel("Username or email address").fill("arewrw");
+  await page.getByLabel("Username or email address").click();
+  await page.getByLabel("Username or email address").fill("username");
+  await page.getByLabel("Password").click();
+  await page.getByLabel("Password").fill("password");
+  await page.getByRole("button", { name: "Sign in" }).click();
+  await page.getByRole("link", { name: "Homepage" }).click();
+  await page.getByPlaceholder("Email address").click();
+  await page.getByPlaceholder("Email address").fill("usename");
+  await page.getByRole("button", { name: "Sign up for GitHub" }).click();
+});
